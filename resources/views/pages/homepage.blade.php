80f35146363b7f2b81c054d354502e3a2ec658a8
FRICK0123 <user.paculbafrick2@gmail.com>
2024-08-22 13:04:53 +0800

    current backup save

--- resources/views/pages/homepage.blade.php
+++ resources/views/pages/homepage.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Masayon Homepage</title>
-    <link rel="stylesheet" href="{{asset('css/homepage.css')}}">
+    <link rel="stylesheet" href="{{ asset('css/homepage.css') }}">
     <link rel="shortcut icon" href="{{ asset('favicon.png') }}">
 
     <!--Bootstrap CDN Links-->
